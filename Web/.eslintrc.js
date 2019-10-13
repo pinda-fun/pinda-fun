c99d76ba3e140db0732c4f7b6e5365f29827e8b0
Julius S <jluuiss@gmail.com>
2019-10-13 21:14:27 +0800

    Add linting (#4)

---
+++ Web/.eslintrc.js
@@ -0,0 +1,39 @@
+const warnInDevelopment = process.env.NODE_ENV === 'production' ? 'error' : 'warn';
+
+module.exports = {
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
+  ],
+  "plugins": ["react", "@typescript-eslint", "prettier"],
+  "env": {
+    "browser": true,
+    "jasmine": true,
+    "jest": true
+  },
+  "rules": {
+    'prettier/prettier': [
+      warnInDevelopment,
+      {
+        'singleQuote': true
+      }
+    ],
+
+    // Allow debugger and console statement in development
+    'no-debugger': warnInDevelopment,
+    'no-console': warnInDevelopment,
+
+    'no-alert': 'off',
+    'prefer-destructuring': 'off'
+  },
+  "settings": {
+    "react": {
+      "pragma": "React",
+      "version": "detect"
+    }
+  },
+  "parser": "@typescript-eslint/parser"
+};
