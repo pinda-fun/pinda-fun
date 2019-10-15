f991bb652a365b8d973e1d08066e40fc65860fe6
hzxcaryn <heng.zhixuan.caryn@dhs.sg>
2019-10-15 14:11:46 +0800

    fix typo

--- Web/src/utils/media.ts
+++ Web/src/utils/media.ts
@@ -1,4 +1,4 @@
-// Contants for media query, taken from bootstrap
+// Constants for media query, taken from bootstrap
 export const smMin = '576px';
 export const mdMin = '768px';
 export const lgMin = '992px';
