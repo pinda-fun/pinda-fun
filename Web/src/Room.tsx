c6e5af76fea4058c5123a518c4ddf3e0ebfaba23
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-10-16 21:46:19 +0800

    Implement initial socket joining capability

--- Web/src/Room.tsx
+++ Web/src/Room.tsx
@@ -1,22 +1,25 @@
-import React, { FunctionComponent, useState, useEffect, useRef } from 'react';
-import { Socket, Channel } from 'phoenix';
+import React, { FunctionComponent, useState, useEffect, useRef } from "react";
+import { Socket, Channel } from "phoenix";
+import uuidv4 from "uuid/v4";
 
-const SOCKET_URL = '/socket';
+const SOCKET_URL = "ws://localhost:4000/socket";
 const TIMEOUT_DURATION = 5000;
 
-const CHANNEL_JOIN_OK = 'Channel %s: Joined channel';
-const CHANNEL_JOIN_ERROR = 'Channel %s: Failed to join';
-const CHANNEL_JOIN_TIMEOUT = 'Channel %s: Timeout';
-const NO_CHANNEL = 'Not connected to any channel';
+const CLIENT_ID_LOCAL_STORAGE_KEY = "client_id";
+
+const CHANNEL_JOIN_OK = "Channel %s: Joined channel";
+const CHANNEL_JOIN_ERROR = "Channel %s: Failed to join";
+const CHANNEL_JOIN_TIMEOUT = "Channel %s: Timeout";
+const NO_CHANNEL = "Not connected to any channel";
 
 interface RoomProps {
   roomId: string;
 }
 
 enum ChannelResponse {
-  OK = 'ok',
-  ERROR = 'error',
-  TIMEOUT = 'timeout'
+  OK = "ok",
+  ERROR = "error",
+  TIMEOUT = "timeout"
 }
 
 enum GameType {
@@ -24,7 +27,7 @@ enum GameType {
 }
 
 enum EventType {
-  GameFinished = 'gameFinishedEvent'
+  GameFinished = "gameFinishedEvent"
 }
 
 abstract class SocketMessage {
@@ -43,7 +46,20 @@ class GameFinishedMessage extends SocketMessage {
   }
 }
 
+const getClientId = () => {
+  const maybeClientId = localStorage.getItem(CLIENT_ID_LOCAL_STORAGE_KEY);
+  if (maybeClientId == null) {
+    const clientId = uuidv4();
+    localStorage.setItem(CLIENT_ID_LOCAL_STORAGE_KEY, clientId);
+    return clientId;
+  } else {
+    return maybeClientId;
+  }
+};
+
 export const Room: FunctionComponent<RoomProps> = (props: RoomProps) => {
+  const clientId = getClientId();
+  const [socket, setSocket] = useState<null | Socket>(null);
   const [channel, setChannel] = useState<null | Channel>(null);
   const channelRef = useRef(channel);
   const [game, setGame] = useState<null | GameType>(null);
@@ -55,12 +71,16 @@ export const Room: FunctionComponent<RoomProps> = (props: RoomProps) => {
   };
 
   /**** METHODS ****/
-  const initConnection = (): void => {
-    const socket = new Socket(SOCKET_URL, { params: {} });
+  const connectSocket = (): void => {
+    const socket = new Socket(SOCKET_URL, { params: { clientId } });
     socket.connect();
+    setSocket(socket);
+  };
 
-    const channel = socket.channel('room:'.concat(props.roomId));
-    channel
+  const changeChannel = () => {
+    if (socket == null) return;
+    const newChannel = socket.channel("room:".concat(props.roomId));
+    newChannel
       .join(TIMEOUT_DURATION)
       .receive(ChannelResponse.OK, handleConnect)
       .receive(ChannelResponse.ERROR, ({ reason }) =>
@@ -70,7 +90,11 @@ export const Room: FunctionComponent<RoomProps> = (props: RoomProps) => {
         console.log(CHANNEL_JOIN_TIMEOUT)
       );
 
-    setChannel(channel);
+    setChannel(newChannel);
+
+    return () => {
+      newChannel.leave();
+    };
   };
 
   const sendMessage = (socketMessage: SocketMessage): void => {
@@ -91,7 +115,8 @@ export const Room: FunctionComponent<RoomProps> = (props: RoomProps) => {
     sendMessage(message);
   };
 
-  useEffect(initConnection, [props.roomId]);
+  useEffect(connectSocket, []);
+  useEffect(changeChannel, [props.roomId]);
 
   return (
     <div>
--- Web/src/Routes.tsx
+++ Web/src/Routes.tsx
@@ -1,13 +1,23 @@
-import React, { lazy, Suspense } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Loading from './components/common/Loading';
+import React, { lazy, Suspense } from "react";
+import { Switch, Route } from "react-router-dom";
+import Loading from "./components/common/Loading";
+import Room from "./Room";
 
-const LandingPage = lazy(() => import('./components/landing'));
+const LandingPage = lazy(() => import("./components/landing"));
+
+const RoomPage: React.FC = () => {
+  return (
+    <div>
+      <Room roomId="lobby" />
+    </div>
+  );
+};
 
 const Routes: React.FC = () => (
   <Suspense fallback={<Loading />}>
     <Switch>
       <Route exact path="/" component={LandingPage} />
+      <Route exact path="/room" component={RoomPage} />
     </Switch>
   </Suspense>
 );
