b2d847dd6d472bb7cf938902cb7f7fb709236268
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-10-18 18:41:37 +0800

    Remove TestNewRoomPage

--- Web/src/Routes.tsx
+++ Web/src/Routes.tsx
@@ -1,7 +1,6 @@
 import React, { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Loading from './components/common/Loading';
-import TestNewRoomPage from './components/room/NewRoomPage';
 import TestJoinRoomPage from './components/room/JoinRoomPage';
 
 const LandingPage = lazy(() => import('components/landing'));
@@ -16,7 +15,6 @@ const Routes: React.FC = () => (
       <Route path="/new" component={CreateRoomPage} />
       <Route path="/join" component={JoinRoomPage} />
       <Route exact path="/balloon-game" component={BalloonShake} />
-      <Route exact path="/test/newroom" component={TestNewRoomPage} />
       <Route exact path="/test/joinroom" component={TestJoinRoomPage} />
     </Switch>
   </Suspense>
