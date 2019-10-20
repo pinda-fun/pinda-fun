9c85ecc960676d38e50404acb604604e634727e1
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-10-20 13:51:23 +0800

    Add ecto to backend (#53)
    * Regenerate with ecto
    
    * Restore root and config/ from master
    
    * Restore api/lib/api
    
    * Restore api/lib/api_web
    
    * Restore mix.exs
    
    * Restore https keys
    
    * Restore api/test/
    
    * Fix travis
    
    * Fix docker related files
    
    * Create backend_db script and move all scripts to its own directory
    
    * Make changes necessary for gigalixir
    
    * Add cd to root directory in deploy scripts
    
    * Only warn on deploying non-master branch
    
    * Change host url to dokku specified env
    
    * Use named mount for postgres

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :api, ApiWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: System.get_env("HOSTNAME"), port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   check_origin: {ApiWeb.Endpoint, :origin_ok?, []}
 
--- lib/api/application.ex
+++ lib/api/application.ex
@@ -8,6 +8,8 @@ defmodule Api.Application do
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
+      # Start the Ecto repository
+      Api.Repo,
       # Start the endpoint when the application starts
       ApiWeb.Endpoint,
       ApiWeb.Presence,
