9f2b5dd28e7ae050fdf9d1b11c25346ec4c0c257
Julius S <jluuiss@gmail.com>
2019-10-22 01:33:20 +0800

    PR Review comments

--- Web/src/components/games/MentalSums/ProblemGen.ts
+++ Web/src/components/games/MentalSums/ProblemGen.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 export interface Problem {
   probAsString: string;
@@ -16,9 +16,9 @@ const generateProblem = (opRand: number, termRand1: number, termRand2: number): 
   const formatted = (num: number) => (num < 0 ? `(${num})` : `${num}`);
   const [opText, opFunc] = operators[opRand % 3];
   const firstTerm = Math.max(termRand1 % 7, termRand1 % 8)
-      * (termRand1 % 2 === 0 ? 1 : -1);
+    * (termRand1 % 2 === 0 ? 1 : -1);
   const secondTerm = Math.max(termRand2 % 5, termRand2 % 4)
-      * (termRand2 % 2 === 0 ? 1 : -1);
+    * (termRand2 % 2 === 0 ? 1 : -1);
   const [t1, t2] = (termRand1 + termRand2) % 2 === 0
     ? [firstTerm, secondTerm] : [secondTerm, firstTerm];
   return {
@@ -28,23 +28,23 @@ const generateProblem = (opRand: number, termRand1: number, termRand2: number): 
 };
 
 export const useQuestionStream = (rng: seedrandom.prng) => {
-  const [_rng] = useState([rng]); // need to box this or else it'll become a number
+  const rand = useRef(rng).current; // need to box this or else it'll become a number
 
   const getProblem = () => generateProblem(
-    Math.abs(_rng[0].int32()),
-    Math.abs(_rng[0].int32()),
-    Math.abs(_rng[0].int32()),
+    Math.abs(rand.int32()),
+    Math.abs(rand.int32()),
+    Math.abs(rand.int32()),
   );
 
-  const [problem, setProblem] = useState<Problem>(getProblem());
+  const [problem, setProblem] = useState(getProblem());
   const [problemText, setProblemText] = useState('');
   const [expectedAns, setExpectedAns] = useState<number | null>(null);
 
   const nextProblem = () => setProblem(getProblem());
 
   useEffect(() => {
-    setProblemText(problem === null ? '' : problem.probAsString);
-    setExpectedAns(problem && problem.expectedRes);
+    setProblemText(problem.probAsString);
+    setExpectedAns(problem.expectedRes);
   }, [problem]);
 
   return { problemText, expectedAns, nextProblem };
