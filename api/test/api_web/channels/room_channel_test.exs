f0b84e8da8eb93da0b1c15313ae083baf83ff0d0
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-10-25 01:10:19 +0800

    Add another non-host

--- api/test/api_web/channels/room_channel_test.exs
+++ api/test/api_web/channels/room_channel_test.exs
@@ -6,6 +6,7 @@ defmodule ApiWeb.RoomChannelTest do
   @game "Shake"
   @host_name "Julius"
   @non_host_name "asd"
+  @non_host2_name "zxc"
 
   test "Lobby returns pin" do
     Enum.each(1..10, fn _ ->
@@ -65,11 +66,22 @@ defmodule ApiWeb.RoomChannelTest do
         assert %{"isHost" => false, "name" => @non_host_name} = non_host_meta
 
         receive do
+          {:non_host2, :connected} -> nil
+        end
+
+        assert_push "presence_diff", %{joins: joins, leaves: leaves}
+        assert Enum.empty?(leaves)
+        assert map_size(joins) == 1
+        assert %{"non-host2" => %{metas: [meta]}} = joins
+        assert %{"isHost" => false, "name" => @non_host2_name} = meta
+
+        receive do
           {:non_host, :start} -> nil
         end
 
         push(socket, "start", %{})
         send(tester_pid, {:host, :non_host, :start})
+        send(tester_pid, {:host, :non_host2, :start})
 
         assert_push "presence_diff", %{
           leaves: %{"host" => %{metas: [%{"isStart" => false}]}},
@@ -82,11 +94,23 @@ defmodule ApiWeb.RoomChannelTest do
 
         push(socket, "stop", %{})
         send(tester_pid, {:host, :non_host, :stop})
+        send(tester_pid, {:host, :non_host2, :stop})
 
         assert_push "presence_diff", %{
           leaves: %{"host" => %{metas: [%{"isStart" => true}]}},
           joins: %{"host" => %{metas: [%{"isStart" => false}]}}
         }
+
+        send(tester_pid, {:host, :non_host2, :disconnect})
+
+        receive do
+          {:non_host2, :disconnected} -> nil
+        end
+
+        assert_push "presence_diff", %{leaves: leaves, joins: joins}
+        assert Enum.empty?(joins)
+        assert map_size(leaves) == 1
+        assert %{"non-host2" => %{metas: [_meta]}} = leaves
 
         send(tester_pid, {:host, :done})
       end)
@@ -132,6 +156,18 @@ defmodule ApiWeb.RoomChannelTest do
         ref = push(socket, "start", %{})
         assert_reply ref, :error, %{reason: "Only host can perform this"}
 
+        send(tester_pid, {:non_host, :non_host2, :connect})
+
+        receive do
+          {:non_host2, :connected} -> nil
+        end
+
+        assert_push "presence_diff", %{leaves: leaves, joins: joins}
+        assert Enum.empty?(leaves)
+        assert map_size(joins) == 1
+        assert %{"non-host2" => %{metas: [meta]}} = joins
+        assert %{"name" => @non_host2_name, "isHost" => false} = meta
+
         send(tester_pid, {:non_host, :host, :start})
 
         receive do
@@ -157,10 +193,69 @@ defmodule ApiWeb.RoomChannelTest do
           joins: %{"host" => %{metas: [%{"isStart" => false}]}}
         }
 
+        receive do
+          {:non_host2, :disconnected} -> nil
+        end
+
+        assert_push "presence_diff", %{leaves: leaves, joins: joins}
+        assert Enum.empty?(joins)
+        assert map_size(leaves) == 1
+        assert %{"non-host2" => %{metas: [_meta]}} = leaves
+
         send(tester_pid, {:non_host, :done})
       end)
 
-    receive_loop(%{host: host_pid, non_host: non_host_pid})
+    {:ok, non_host2_pid} =
+      Task.start_link(fn ->
+        receive do
+          {:non_host, :connect} -> nil
+        end
+
+        assert {:ok, %{}, socket} =
+                 socket(ApiWeb.UserSocket, "", %{client_id: "non-host2"})
+                 |> join(RoomChannel, "room:#{pin}", %{"name" => @non_host2_name})
+
+        # Also check that only 1 connection per client is allowed
+        assert {:error, %{reason: "Existing connection"}} =
+                 socket(ApiWeb.UserSocket, "", %{client_id: "non-host2"})
+                 |> join(RoomChannel, "room:#{pin}", %{"name" => @non_host2_name})
+
+        send(tester_pid, {:non_host2, :non_host, :connected})
+        send(tester_pid, {:non_host2, :host, :connected})
+
+        receive do
+          {:host, :start} -> nil
+        end
+
+        assert_push "presence_diff", %{
+          leaves: %{"host" => %{metas: [%{"isStart" => false}]}},
+          joins: %{"host" => %{metas: [%{"isStart" => true}]}}
+        }
+
+        receive do
+          {:host, :stop} -> nil
+        end
+
+        assert_push "presence_diff", %{
+          leaves: %{"host" => %{metas: [%{"isStart" => true}]}},
+          joins: %{"host" => %{metas: [%{"isStart" => false}]}}
+        }
+
+        receive do
+          {:host, :disconnect} -> nil
+        end
+
+        Process.unlink(socket.channel_pid)
+        ref = leave(socket)
+        assert_reply ref, :ok
+
+        send(tester_pid, {:non_host2, :host, :disconnected})
+        send(tester_pid, {:non_host2, :non_host, :disconnected})
+
+        send(tester_pid, {:non_host2, :done})
+      end)
+
+    receive_loop(%{host: host_pid, non_host: non_host_pid, non_host2: non_host2_pid})
   end
 
   defp receive_loop(mapping, done_set \\ MapSet.new()) when is_map(mapping) do
