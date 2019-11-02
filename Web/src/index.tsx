12d1ac4f08b3902d85ec0e7d5ae17303ca055d6a
hzxcaryn <heng.zhixuan.caryn@dhs.sg>
2019-11-02 21:41:59 +0800

    fix lint

--- Web/src/index.tsx
+++ Web/src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import smoothscroll from 'smoothscroll-polyfill';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 // Polyfills
-import smoothscroll from 'smoothscroll-polyfill';
 
 smoothscroll.polyfill();
 
