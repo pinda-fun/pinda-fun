fe7133543424742d345a06bbe69639c5961ba979
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-11-03 19:07:35 +0800

    Remove test

--- Web/config-overrides.js
+++ Web/config-overrides.js
@@ -1,7 +1,14 @@
 module.exports = {
   webpack: (config) => {
     config.resolve.alias.react = 'preact/compat';
+    config.resolve.alias['react-dom/test-utils'] = 'preact/test-utils';
     config.resolve.alias['react-dom'] = 'preact/compat';
+    return config;
+  },
+  jest: (config) => {
+    config.moduleNameMapper.react = 'preact/compat';
+    config.moduleNameMapper['react-dom/test-utils'] = 'preact/test-utils';
+    config.moduleNameMapper['react-dom'] = 'preact/compat';
     return config;
   }
 }
