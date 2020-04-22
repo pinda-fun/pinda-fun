f16ee1256712af46a0f7c836b79a9a38b78db76d
Liang Jun <au.liangjun@gmail.com>
2020-04-22 10:10:47 +0800

    Add Netlify badge

---
+++ Web/src/components/common/NetlifyBadge.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NetlifyBadge: React.FC = () => (
+  <a href="https://www.netlify.com">
+    <img
+      src="https://www.netlify.com/img/global/badges/netlify-light.svg"
+      alt="Deploys by Netlify"
+    />
+  </a>
+);
+
+export default NetlifyBadge;
--- Web/src/components/landing/NavBar.tsx
+++ Web/src/components/landing/NavBar.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { Link } from 'react-router-dom';
 import Button from '../common/Button';
+import NetlifyBadge from '../common/NetlifyBadge';
 
 const NavBarContainer = styled.nav`
   background: transparent;
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: flex-end;
+  justify-content: space-between;
   padding: 1rem;
 `;
 
 const NavBar: React.FC = () => (
   <NavBarContainer>
+    <NetlifyBadge />
     <Link to={{ pathname: '/join' }}>
       <Button primary>Join Game</Button>
     </Link>
