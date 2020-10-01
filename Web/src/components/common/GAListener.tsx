59664ae5f27ebb52e09ef037fd9c2e2170e455cf
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2020-10-01 17:03:17 +0800

    Revert "Bump history"
    This reverts commit c02fd6f0da8fb52b3cf3b89cdf0ede324e01fccc.

--- Web/src/components/common/GAListener.tsx
+++ Web/src/components/common/GAListener.tsx
@@ -1,12 +1,12 @@
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 import React, { useEffect } from 'react';
 import ReactGA from 'react-ga';
-import { Update } from 'history';
+import { Location } from 'history';
 import getClientId from 'utils/getClientId';
 
 const TRACKING_ID = 'UA-151199787-1';
 
-function sendPageView({ location }: Update) {
+function sendPageView(location: Location) {
   ReactGA.set({ page: location.pathname });
   ReactGA.pageview(location.pathname);
 }
@@ -14,10 +14,7 @@ function sendPageView({ location }: Update) {
 const GAListener: React.FC<RouteComponentProps> = ({ children, history }) => {
   useEffect(() => {
     const testMode = process.env.NODE_ENV === 'test';
-    ReactGA.initialize(TRACKING_ID, {
-      gaOptions: { clientId: getClientId() },
-      testMode,
-    });
+    ReactGA.initialize(TRACKING_ID, { gaOptions: { clientId: getClientId() }, testMode });
     return history.listen(sendPageView);
   }, [history]);
 
