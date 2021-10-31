2626af7a09bc451703c678ad04ed8952c00b12e6
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2021-10-31 20:24:34 +0800

    Fix typecheck error

--- Web/src/components/common/GAListener.tsx
+++ Web/src/components/common/GAListener.tsx
@@ -1,12 +1,12 @@
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 import React, { useEffect } from 'react';
 import ReactGA from 'react-ga';
-import { Location } from 'history';
+import { Update } from 'history';
 import getClientId from 'utils/getClientId';
 
 const TRACKING_ID = 'UA-151199787-1';
 
-function sendPageView(location: Location) {
+function sendPageView({ location }: Update) {
   ReactGA.set({ page: location.pathname });
   ReactGA.pageview(location.pathname);
 }
