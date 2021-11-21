891e1ee2dad1fd891c499636f79fe554af290137
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2021-11-21 17:42:31 +0800

    Fix react-ga to play better with react-router (#1310)

---
+++ Web/src/utils/usePageTracking.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import ReactGA from 'react-ga';
+import { useLocation } from 'react-router-dom';
+import getClientId from 'utils/getClientId';
+
+const TRACKING_ID = 'UA-151199787-1';
+
+const usePageTracking = () => {
+  const [initialised, setInitialised] = useState(false);
+  useEffect(() => {
+    if (window.location.origin.includes('pinda.fun')) {
+      ReactGA.initialize(TRACKING_ID, { gaOptions: { clientId: getClientId() } });
+      setInitialised(true);
+    }
+  }, []);
+
+  const location = useLocation();
+  useEffect(() => {
+    if (initialised) {
+      ReactGA.set({ page: location.pathname });
+      ReactGA.pageview(location.pathname);
+    }
+  }, [initialised, location]);
+};
+
+export default usePageTracking;
