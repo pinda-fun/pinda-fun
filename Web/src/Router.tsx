f5717abdf3de4412dd2683e53b4633b35882fdea
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2022-11-14 09:25:13 +0800

    Move react-router out of the main bundle (#1704)

---
+++ Web/src/Router.tsx
@@ -0,0 +1,10 @@
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
+
+const Router = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
+
+export default Router;
